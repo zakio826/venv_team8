e66b09d22403d4b6105f50005d7871437ce5c603
zakio826 <fki2266287@stu.o-hara.ac.jp>
2023-09-25 14:33:13 +0900

    データベース設定の続き

--- Django/item_tracker/item_tracker/wsgi.py
+++ Django/item_tracker/item_tracker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'item_tracker.settings_dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'item_tracker.settings')
 
 application = get_wsgi_application()
