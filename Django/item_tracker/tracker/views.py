be5dcfb53c0a9549aa0b78ea4b9603c7cbccbeca
kawamura <fki2266219@stu.o-hara.ac.jp>
2023-10-20 09:45:49 +0900

    kousinn

--- Django/item_tracker/tracker/views.py
+++ Django/item_tracker/tracker/views.py
@@ -1261,3 +1261,6 @@ class HistoryDetailView(LoginRequiredMixin, generic.DetailView):
     model = History
     template_name = 'history_detail.html'  # このテンプレートは後で作成します
     context_object_name = 'history'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
